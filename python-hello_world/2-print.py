22ff7cf50148b0c9a0a3aa106bf850df561d1f0b
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-17 11:51:34 -0400

    New line

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,6 @@
+#!/user/bin/python3
+
+# Write a script that print "Programming is like building a multilingual puzzle" followed by and new line
+
+print('Programming is like building a multilingual puzzle')
+print()
