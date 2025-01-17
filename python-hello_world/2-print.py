17de340a56e8caaf5b4d07a70145f819e26e4815
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-17 12:08:53 -0400

    fixed code

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -5,7 +5,5 @@ This can be done in two ways: using /n during the prompt or after.
 '''
 
 print('Programming is like building a multilingual puzzle')
-print()  # Path one (adds a new line)
-# Example (Path two), not intended to print anything:
-# print('Hello World\\how was your day today')  # This is just an example
+print()  # This adds the required newline
 
