90e5602327086ecdbcf6a6823e90a8968f06cb75
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-17 12:04:10 -0400

    fixed code

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,8 +1,9 @@
 #!/user/bin/python3
-
-# Write a script that print "Programming is like building a multilingual puzzle" followed by and new line
-# This can be done in two ways using /n durint the prompt or after 
+'''
+ Write a script that print "Programming is like building a multilingual puzzle" followed by and new line
+This can be done in two ways using /n durint the prompt or after 
+'''
 print('Programming is like building a multilingual puzzle')
 print() # Path one
 print('this is a new line')
-# print('Hello World\how was your day today') # Path two
+" rint('Hello World\how was your day today') # Path two"
