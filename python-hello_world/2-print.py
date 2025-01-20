4b6ddde25022c205769f5c914031116fae03a8be
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-19 20:37:28 -0400

    fixed file

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,9 +1,2 @@
-#!/usr/bin/python3
-'''
-Write a script that prints "Programming is like building a multilingual puzzle" followed by a new line.
-This can be done in two ways: using /n during the prompt or after.
-'''
-
-print('Programming is like building a multilingual puzzle')
-print()  # This adds the required newline
-
+#!user/bin/python3
+print("\"Programming is like building a multilingual puzzle")
