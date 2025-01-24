7af535c4436e0ef5bbe81afea439c2608a25f161
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-24 11:00:56 -0400

    fix document

---
+++ python-data_structures/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
+'''
+This file prints out the number [1,2,3,4,5] like this:
+    1
+    2
+    3
+    4
+    5
+'''
--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,14 @@
 #!user/bin/python3
 
-import random
-import math
-
-
-OneToFive = list(range(6))
-
-print(*OneToFive, sep="\n")
+def print_list_integer(my_list=[]):
+    '''
+    Prints the number 1 - 5 using the Main file that def My_list
+    The out put should be like this:
+    1
+    2
+    3
+    4
+    5
+    '''
+    for i in my_list:
+        print(str(), format(i))
