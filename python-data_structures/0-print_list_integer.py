4bbe374ad112a240b51b1290bffae8cf7e6c8895
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-26 21:04:51 -0400

    code fix again

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,7 @@
-#!user/bin/python3
+#!bin/user/python3
 
-# Use Print_List_integer(MyList[]) to comeplet the code in the 0-Main.py your only allowed it use str().format()
-"Prints out the number with a new line starting from 1 then ends with 5"
-
-def print_list_integer(mylist=[]):
-    for i in mylist:
-        print("{:d}" .format(i))
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        # Use str.format() to print each integer
+        print("{:d}".format(i))
 
