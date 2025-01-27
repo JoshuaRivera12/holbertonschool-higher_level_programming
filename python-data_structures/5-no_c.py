61f79676607beaf39a809c5d91cf9865b1645463
JoshuaRivera12 <lobitrivera@gmail.com>
2025-01-26 21:42:32 -0400

    fixed code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char !='c' and char != 'C':
+        if char != 'c' and char != 'C':
             new_string += char
-        return new_string
+    return new_string  # Ensure this is outside the loop
+
