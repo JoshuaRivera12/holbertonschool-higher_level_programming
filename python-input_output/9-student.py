90822c32e6901ca5d1f861d146c23f64fb0b9229
JoshuaRivera12 <lobitrivera@gmail.com>
2025-02-14 11:38:39 -0400

    fixed file

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""class to json"""
+"""
+Defines a Student class.
+"""
 
 
-def class_to_json(obj):
-    """class to json"""
-    return obj.__dict__
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+        return self.__dict__
