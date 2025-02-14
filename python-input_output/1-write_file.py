524a4dca8da84d39d6aebf7d7a56ae7149451385
JoshuaRivera12 <lobitrivera@gmail.com>
2025-02-14 10:52:08 -0400

    fixed pep

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-    Writes a string to a text file
+Writes a string to a text file.
 """
 
 def write_file(filename="", text=""):
-    """open a txt file and if not extist create it"""
+    """Opens a text file, creates it if it doesn't exist, and writes a string to it."""
     with open(filename, 'w', encoding='utf-8') as file:
-               return file.write(text)
+        return file.write(text)
+
