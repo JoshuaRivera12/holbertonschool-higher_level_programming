9c7231e9f416f7133457013816d5a1a56d553bd4
JoshuaRivera12 <lobitrivera@gmail.com>
2025-02-14 10:47:02 -0400

    adding coded file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""
+    Writes a string to a text file
+"""
+
+def write_file(filename="", text=""):
+    """open a txt file and if not extist create it"""
+    with open(filename, 'w', encoding='utf -8') as file:
+               return file.write(text)
