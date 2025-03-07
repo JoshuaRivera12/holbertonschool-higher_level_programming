0ad4a4255694e03e03e79e7a8fe44fb6530ab8f1
JoshuaRivera12 <lobitrivera@gmail.com>
2025-03-07 19:06:24 -0400

    test2

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,26 +7,8 @@ import sys
 
 
 if __name__ == "__main__":
-    # Verification of all required arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-
-    # Command line args
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connection to MySQL Server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
     # Creating Cursor
     cursor = db.cursor()
 
