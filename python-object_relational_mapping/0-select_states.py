50d5ad2a1210e1bd8a74604f2024b875f0546d42
JoshuaRivera12 <lobitrivera@gmail.com>
2025-03-07 12:49:21 -0400

    fixed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Lists all states from the database hbtn_0e_0_usa.
 """
@@ -7,35 +7,42 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Ensure correct number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: python3 {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     # Get MySQL credentials from command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
-    # Connect to the MySQL database
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-    
-    # Create a cursor object to interact with the database
-    cur = conn.cursor()
-    
-    # Execute SQL query to retrieve all states sorted by id
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-    
-    # Fetch all results
-    query_rows = cur.fetchall()
-    
-    # Print results row by row
-    for row in query_rows:
-        print(row)
-    
-    # Close cursor and connection
-    cur.close()
-    conn.close()
+
+    try:
+        # Connect to the MySQL database
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8"
+        )
+
+        # Create a cursor object to interact with the database
+        cur = conn.cursor()
+
+        # Execute SQL query to retrieve all states sorted by id
+        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+        # Fetch and print results
+        for row in cur.fetchall():
+            print(row)
+
+        # Clean up
+        cur.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print("Error: {}".format(e))
+        sys.exit(1)
 
