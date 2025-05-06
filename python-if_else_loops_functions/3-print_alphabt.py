7eeb3ee3792c124fd9ba40ba57dbf57a0d43136a
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-06 11:54:00 -0400

    fix smt

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
 # The code prints the lowercase alphabet except for 'e' and 'q'
