5fb9b0578206046262ef74265933b11eb8cab33f
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-06 11:47:31 -0400

    checking files

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for i in range(ord('a'), ord('z') + 1):
     if i != ord('e') and i != ord('q'):
         print("{}".format(chr(i)), end="")
-# # The code prints the lowercase alphabet except for 'e' and 'q'
+# The code prints the lowercase alphabet except for 'e' and 'q'
