5c93947d3a28079d0bdf1e8329a6a821910657bb
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-06 12:46:31 -0400

    change the numbers into letters

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in "abcdefghijklmnopqrstuvwxyz":
-    if letter != 113 and letter != 101:  # ASCII values for 'q' and 'e'
+    if letter != 'e' and letter != 'q':  # ASCII values for 'q' and 'e'
         print("{}".format(letter), end="")
