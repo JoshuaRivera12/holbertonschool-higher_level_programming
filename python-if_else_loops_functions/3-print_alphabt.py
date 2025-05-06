067a148263240e5cb0eabe03d944ef0870bc1eb0
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-06 11:41:24 -0400

    made the code one loop only

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range((ord('a')), (ord('z') + 1)):
-    if i == ord('e') or i == ord('q'):
-        continue
-    print("{}".format(chr(i)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('e') and i != ord('q'):
+        print("{}".format(chr(i)), end="")
 # # The code prints the lowercase alphabet except for 'e' and 'q'
