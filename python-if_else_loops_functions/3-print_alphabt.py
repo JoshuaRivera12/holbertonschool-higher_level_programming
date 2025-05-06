c3b57f10a754fa7a857f11032b56e171a088ae76
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-06 11:50:56 -0400

    changed code to see if it will fix the checkers

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('e') and i != ord('q'):
+for i in range(97, 123):
+    if chr(i) != 'e' and chr(i) != 'q':
         print("{}".format(chr(i)), end="")
 # The code prints the lowercase alphabet except for 'e' and 'q'
