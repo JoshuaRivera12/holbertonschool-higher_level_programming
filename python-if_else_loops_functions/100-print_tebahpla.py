abbc515b59f7222f800c221ce91d9df019b51482
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-07 12:36:50 -0400

    redone the code

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}".format(chr(i)), end="")
+i = 122
+toggle = 0
+while i >= 97:
+    if toggle == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i).upper()), end="")
+    toggle = 1 - toggle
+    i -= 1
