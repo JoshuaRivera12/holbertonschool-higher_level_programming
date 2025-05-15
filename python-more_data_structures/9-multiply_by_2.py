453c46a82aba0f788258612703939ca2df0c0d75
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-15 15:23:38 -0400

    fisnihed task 9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def mulitply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.iteam():
+        value * 2
+        new_dict[key] = value * 2
+    return new_dict
