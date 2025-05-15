2f598a828ee63a068518d4f26fd631adcd771843
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-15 16:13:08 -0400

    fixed number 10 code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
     best_value = None
 
     for key, value in a_dictionary.items():
-        if value > best_value:
+        if best_value is None or value > best_value:
             best_key = key
             best_value = value
     return best_key
