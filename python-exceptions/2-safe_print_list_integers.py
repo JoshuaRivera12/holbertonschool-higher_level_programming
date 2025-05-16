91f41bde4720f3d0b11a7b0d892826821723d764
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-16 12:45:06 -0400

    test test

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 v += 1
             except (ValueError, TypeError):
                 continue
+            except IndexError:
+                break
 
     print()
     return v
