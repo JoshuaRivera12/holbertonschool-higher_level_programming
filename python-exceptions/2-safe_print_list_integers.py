befbaf9658a4c16cac52518c218fcff038760ec0
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-16 12:59:17 -0400

    tempt 500

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
 ### Safe print prints the intergers except a character ###
-    v = 0
+    count = 0
     for i in range(x):
             try:
                 print('{:d}'.format(my_list[i]), end="")
-                v += 1
+                count += 1
             except (ValueError, TypeError):
                 continue
             except IndexError:
                 break
     print()
-    return v
+    return count
