a91bc01065cc4416677ea476b9bf95b9066ae8b0
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-22 16:03:33 -0400

    fixed pycodestyle

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
+
+
 class Square:
     def __init__(self, size=0):
+
         self.__size = size
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
 
-def area(self):
-    return self.__size * self.__size
+    def area(self):
+        return self.__size * self.__size
 
-@property
-def size(self):
-    return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-@size.setter
-def size(self, value):
-    if type(value) != int:
-        raise TypeError("size must be an integer")
-    if value < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = value
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
 
-def my_print(self):
-    if self.__size == 0:
-        print()
-    else:
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+
+        for number in range(self.__size):
+            print("#" * self.__size)
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""" 6. Coordinates of a square
-A class Square that defines a square by: (based on 5-square.py)
-"""
 
 
 class Square:
-    """Class Square that defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        """Define Private instance attribute: size"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Private instance attribute: size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Private instance attribute: size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -27,12 +20,10 @@ class Square:
 
     @property
     def position(self):
-        """Private instance attribute: position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Private instance attribute: position"""
         if type(value) is not tuple or len(value) != 2 or \
            type(value[0]) is not int or type(value[1]) is not int or \
            value[0] < 0 or value[1] < 0:
@@ -40,11 +31,9 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Returns the current square area"""
         return self.__size * self.__size
 
     def my_print(self):
-        """Prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
         else:
@@ -55,8 +44,6 @@ class Square:
                 print("#" * self.__size)
 
     def pos_print(self):
-        """"Function that returns the position of the square"""
-
         s_position = ""
         if self.size == 0:
             return "\n"
@@ -76,5 +63,4 @@ class Square:
         return s_position
 
     def __str__(self):
-        """Prints in stdout the square with the character #"""
         return self.pos_print()
