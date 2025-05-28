1699e23aff8b7059a84285f6c155913530803100
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-28 13:52:06 -0400

    fixed?

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" This class sorts all files into a numarical order"""
+
 
 class MyList(list):
+    """this def make the files sorted"""
     def print_sorted(self):
         print(sorted(self))
-
