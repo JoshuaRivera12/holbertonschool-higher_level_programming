c917b4993f2e5d5a4ffe988e7d2ac05dc20eaa45
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-28 14:13:47 -0400

    test test test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,5 +5,5 @@
 class MyList(list):
     """this def make the files sorted"""
     def print_sorted(self):
-       "" "this prints the list"""
+       """this prints the list"""
         print(sorted(self))
