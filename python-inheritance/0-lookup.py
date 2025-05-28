739529e1f94e1649097aacc9c0db28cb73197d8d
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-28 13:45:29 -0400

    files

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This module define a funtion that returns the list
+"""
+
+
+def lookup(obj):
+    """returtns the list of availbe attributes and methods of obj."""
+    return dir(obj)
