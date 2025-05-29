c2ff2e1ec20a32f703d93f631e3d70adfd2d2db1
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-29 11:25:45 -0400

    testing

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" this task is to see if the obj is == a_class and return true if it is and false if its not """
+""" this task is to see if the obj is == a_class"""
 
 
 def is_same_class(obj, a_class):
-    """this returns the value to check if it is or not """
-    return type(obj) == a_class
+    """this returns the value"""
+    return type(obj) is a_class
