91a9f8ae3ad531c7cad5f17589c9a42ae8b29c2a
JoshuaRivera12 <lobitrivera@gmail.com>
2025-05-29 14:09:33 -0400

    fixed file

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" this file is for the class geomerty """
+
+
 class BaseGeometry:
     """
     This is the base class for geometry objects.
     """
+
 
 class BaseGeometry:
     """ Base Geometry"""
