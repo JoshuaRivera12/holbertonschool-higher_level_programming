e89587afd40ea6a0a6a7f1701769013ce101031b
JoshuaRivera12 <lobitrivera@gmail.com>
2025-06-03 13:36:03 -0400

    fixed?

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ This code will be able to read the content of a txt file
 
 
 def read_file(filename=""):
+    """
+    in this code it will print out the content of the file
+    """
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
