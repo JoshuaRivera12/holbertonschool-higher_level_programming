780b01e84c953b9a86de5e4dde871f50c7cc092c
JoshuaRivera12 <lobitrivera@gmail.com>
2025-06-05 11:53:26 -0400

    adding files

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""
+This File will write files 
+""
+
+
+def write_file(filename="", text=""):
+    """ this code will write files """
+    with open(filename, "w", encoding="utf-8") as f:
+    return f.write(text)
