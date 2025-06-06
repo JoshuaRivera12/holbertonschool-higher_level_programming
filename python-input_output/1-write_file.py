f015596a33f7f6403cd2a6abb1d6f95d4aa86bc1
JoshuaRivera12 <lobitrivera@gmail.com>
2025-06-06 14:46:20 -0400

    fixing files

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""
-This File will write files 
-""
+""" This File will write files """
 
 
 def write_file(filename="", text=""):
     """ this code will write files """
     with open(filename, "w", encoding="utf-8") as f:
-    return f.write(text)
+        return f.write(text)
