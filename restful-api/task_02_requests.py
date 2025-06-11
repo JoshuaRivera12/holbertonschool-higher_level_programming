ce89106dd442114d80d2ea384fb71d66d7ed1bfd
JoshuaRivera12 <lobitrivera@gmail.com>
2025-06-10 22:09:40 -0400

    adding working files

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+This Functiion will be able to get infromations from API's using request annd CURL
+"""
+
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    responce = requests.get("https://jsonplaceholder.typicode.com/")
+    responce.status_code
+    print("Status Code:", responce.status_code)
+
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        data = response.json()
+        posts_list = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in data
+        ]
+
+        with open("posts.csv", "w", newline='', encoding='utf-8') as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(posts_list)
+
+
+            for post in data:
+                print(post["title"])
+
