16fa3c53c3e934b7d1dffba945173a960f19e664
JoshuaRivera12 <lobitrivera@gmail.com>
2025-06-10 22:33:18 -0400

    test test test

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
 """
-This Functiion will be able to get infromations from API's using request annd CURL
+This function will be able to get information from APIs using requests and CURL
 """
-
 
 import requests
 import csv
 
 
 def fetch_and_print_posts():
-    responce = requests.get("https://jsonplaceholder.typicode.com/posts")
-    responce.status_code
-    print("Status Code:", responce.status_code)
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("Status Code:", response.status_code)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data:
+            print(post["title"], flush=True)  # print titles as expected
 
 
 def fetch_and_save_posts():
@@ -30,8 +32,4 @@ def fetch_and_save_posts():
 
             writer.writeheader()
             writer.writerows(posts_list)
-
-
-            for post in data:
-                print(post["title"], flush=True)
 
