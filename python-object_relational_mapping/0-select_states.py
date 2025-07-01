e412b06faea75cdc105209cd148e5b076ab0514d
JoshuaRivera12 <lobitrivera@gmail.com>
2025-07-01 13:07:43 -0400

    adding LATE files

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Module for listing all states from a database
+"""
+
+
+
+import MySQLdb
+import sys
+
+
+
+def main():
+    """
+    Main function to connect to the database and list all states
+    """
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <username> <password> <database>")
+        sys.exit(1)
+
+    try:
+        connect = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                password=sys.argv[2],
+                database=sys.argv[3]
+                )
+
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+            for row in cursor.fetchall():
+                print(row)
+
+            cursor.close()
+            connection.close()
+
+        except MySQLdb.Error as e:
+            print(f"Error: {e}")
+            sys.exit(1)
+
+
+    if __name__ == "__main__":
+        main()
