ff80d9ddac4e3f0baf75ec126a192e506ac41b55
JoshuaRivera12 <lobitrivera@gmail.com>
2025-07-10 16:54:13 -0400

    adding files

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+if (isNaN(a)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + a);
+}
---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+if (isNaN(a)) {
+  console.log('Missing number of occurrences');
+}
+for (let a = 0; a < args; a++) {
+  console.log('C is fun');
+}
